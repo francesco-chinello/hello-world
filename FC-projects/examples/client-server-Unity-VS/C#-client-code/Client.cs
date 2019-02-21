c9da05139023d255b662e2908ac4c2056ade0036
francesco-chinello <chinello@btech.au.dk>
2019-02-21 12:57:18 +0100

    client-server-unity

---
+++ FC-projects/examples/client-server-Unity-VS/C#-client-code/Client.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    #region private members 	
+    private TcpClient socketConnection;
+    private Thread clientReceiveThread;
+    #endregion
+    // Use this for initialization 	
+    void Start()
+    {
+        try
+        {
+            clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+            clientReceiveThread.IsBackground = true;
+            clientReceiveThread.Start();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("On client connect exception " + e);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SendMessage();
+            // Thread.Sleep(100);
+            ReceiveMessage();
+
+        }
+     
+    }
+
+    /// <summary> 	
+    /// Runs in background clientReceiveThread; Listens for incomming data. 	
+    /// </summary>     
+    private void ListenForData()
+    {
+        try
+        {
+            socketConnection = new TcpClient("localhost", 4444);
+            Byte[] bytes = new Byte[1024];
+           // while (true)
+            //{
+                // Get a stream object for reading 				
+                using (NetworkStream stream = socketConnection.GetStream())
+                {
+                    int length;
+                    // Read incomming stream into byte arrary. 					
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        var incommingData = new byte[length];
+                        Array.Copy(bytes, 0, incommingData, 0, length);
+                        // Convert byte array to string message. 						
+                        string serverMessage = Encoding.ASCII.GetString(incommingData);
+                        Debug.Log("server message received as: " + serverMessage);
+                    }
+                }
+            //}
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+
+    /// <summary> 	
+    /// Send message to server using socket connection. 	
+    /// </summary> 	
+    /// 
+    
+    private void SendMessage()
+    {
+        if (socketConnection == null)
+        {
+            return;
+        }
+        try
+        {
+            // Get a stream object for writing. 			
+            NetworkStream stream = socketConnection.GetStream();
+
+            if (stream.CanWrite)
+            {
+                string clientMessage = "This is a message from one of your clients.";
+                // Convert string message to byte array.                 
+                byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage);
+                // Write byte array to socketConnection stream.                 
+                stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
+                Debug.Log("Client sent his message - should be received by server");
+            }
+        }
+        catch (SocketException socketException)
+        {
+            Debug.Log("Socket exception: " + socketException);
+        }
+    }
+
+
+    private void ReceiveMessage()
+    {
+        using (NetworkStream stream = socketConnection.GetStream())
+        {
+            Byte[] bytes = new Byte[128];
+            int bytesRead = stream.Read(bytes, 0, socketConnection.ReceiveBufferSize);
+
+            Debug.Log("taking the message ... ");
+            Debug.Log("Received : " + Encoding.ASCII.GetString(bytes, 0, bytesRead));
+            
+            /*
+            int length;
+                
+                // Read incomming stream into byte arrary. 	
+                Debug.Log("waiting for message  from server... ");
+
+                while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    Debug.Log( "taking the message ... ");
+
+                    var incommingData = new byte[length];
+                    Array.Copy(bytes, 0, incommingData, 0, length);
+                    // Convert byte array to string message. 						
+                    string serverMessage = Encoding.ASCII.GetString(incommingData);
+                    Debug.Log("server message received as: " + serverMessage);
+                }
+                */
+        }
+            
+
+
+    }// Check to see if this NetworkStream is readable.
+     
+
+}
---
+++ FC-projects/examples/client-server-Unity-VS/vs-server-demo/server-demo/server-demo/server-demo.cpp
@@ -0,0 +1,200 @@
+// server-demo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string.h>
+
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "4444"
+
+char buff_value[18];
+
+void pack_send(int *);
+
+int __cdecl main(void)
+{
+	WSADATA wsaData;
+	int iResult;
+
+	SOCKET ListenSocket = INVALID_SOCKET;
+	SOCKET ClientSocket = INVALID_SOCKET;
+
+	struct addrinfo *result = NULL;
+	struct addrinfo hints;
+
+	int iSendResult;
+	char recvbuf[DEFAULT_BUFLEN];
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed with error: %d\n", iResult);
+		return 1;
+	}
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	// Resolve the server address and port
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0) {
+		printf("getaddrinfo failed with error: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+
+	// Create a SOCKET for connecting to server
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		printf("socket failed with error: %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+
+	if (iResult == SOCKET_ERROR) {
+		printf("bind failed with error: %d \n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result);
+	printf("listening for clients on port ... %d \n", DEFAULT_PORT);
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		printf("listen failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// Accept a client socket
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET) {
+
+		printf("accept failed with error: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+
+	}
+
+	printf("Connected on port ... %d \n", DEFAULT_PORT);
+
+
+	// No longer need server socket
+	closesocket(ListenSocket);
+
+	printf("Ready to receive data on port ... %d \n", DEFAULT_PORT);
+
+	int counter = 1;
+	// Receive until the peer shuts down the connection
+	do {
+		int sendbuf[3]; sendbuf[0] = 111; sendbuf[1] = 222; sendbuf[2] = 333;
+
+		// pack_send(sendbuf); still to debug this
+
+		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+
+		printf("Step number: %d \n", counter);
+		recvbuf[5] = '#';
+		
+		if (iResult > 0) {
+			printf("Bytes received: %d\n", iResult);
+
+			// Echo the buffer back to the sender
+			iSendResult = send(ClientSocket, recvbuf, iResult, 0);
+
+			if (iSendResult == SOCKET_ERROR) {
+				printf("send failed with error: %d\n", WSAGetLastError());
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+			printf("Bytes sent: %d , %d \n", iSendResult, counter);
+			counter++;
+		}
+		else if (iResult == 0)
+			printf("Connection closing...\n");
+		else {
+			printf("recv failed with error: %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+
+	} while (iResult > 0);
+
+	// shutdown the connection since we're done
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR) {
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+
+
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
+}
+
+void pack_send(int * sendbuff) {
+
+	char buff_values[128];
+
+	char c_temp[6]; // temporary 5 char array
+
+	for (int i = 0; i <= 2; i++) {
+
+		_itoa_s(sendbuff[i], c_temp, 10); // converts the number into string
+
+		if (i < 2) {
+
+			c_temp[5] = '/';
+
+		}
+		else {
+
+			c_temp[5] = '#';
+
+		}
+
+		strcat_s(buff_values, c_temp); // da correggere fa una eccezione!!!
+
+	}
+
+	printf("buff values: %s \n", buff_values);
+
+}
+	
+	
+
